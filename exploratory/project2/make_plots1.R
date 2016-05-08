317bdda956c6039354e545e846fa76c7a038bf53
Ziggy Duke <ziggyduke@github.com>
2016-05-09 00:20:36 +0200

    Removed uneccesary files

--- exploratory/project2/make_plots1.R
+++
@@ -1,4 +0,0 @@
-emiyear <- select(NEI, Emissions, year)
-emi_group <- group_by(emiyear, year)
-emisum <- summarize(emi_group, s=sum(Emissions))
-plot(emisum)
--- exploratory/project2/make_plots2.R
+++
@@ -1,7 +0,0 @@
-
-baltimore <- filter(NEI, fips == "24510")
-baltimore <- select(baltimore, Emissions, year)
-
-baltimore_group <- group_by(baltimore, year)
-baltimoresum <- summarize(baltimore_group, total_emissions=sum(Emissions))
-plot(baltimoresum)
